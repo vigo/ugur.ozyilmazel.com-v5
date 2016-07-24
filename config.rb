f6e9f9ad7f20c14857f5d5dfcf6c9bfb58bc9dc0
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-07-24 14:43:37 +0300

    deployment available

--- config.rb
+++ config.rb
@@ -85,11 +85,6 @@ configure :build do
   activate :minify_javascript
 end
 
-# activate :deploy do |deploy|
-#   deploy.deploy_method = :git
-#   # Optional Settings
-#   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-#   # deploy.branch   = 'custom-branch' # default: gh-pages
-#   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-#   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-# end
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+end
